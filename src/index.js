8cef2adc8dcb23a9eb2443365bc7728498d14e30
Shahrukh, Shaik <shahrukh690432@gmail.com>
2023-01-22 22:17:47 +0530

    service worker unregistered

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ root.render(
     <App />
 );
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 
 // If you want to start measuring performance in your app, pass a function
